112aafc22b0bbf1c7c6940cd2a2f984c1824098d
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-07-24 18:40:02 +0600

    update

--- src/components/Login & Signup/GoGiFa.jsx
+++ src/components/Login & Signup/GoGiFa.jsx
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 
 const GoGiFa = () => {
 
-    const { signInWithGoogle } = useContext(AuthContext);
+    const { signInWithGoogle, signInWithGithub } = useContext(AuthContext);
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -42,14 +42,46 @@ const GoGiFa = () => {
             })
     }
 
+    const handleGithubSignIn = () => {
+        signInWithGithub()
+            .then(result => {
+                const loggedUser = result.user;
+                if (loggedUser) {
+                    Swal.fire({
+                        title: 'Login Success!! Enjoy Your Time',
+                        showClass: {
+                            popup: 'animate__animated animate__fadeInDown'
+                        },
+                        hideClass: {
+                            popup: 'animate__animated animate__fadeOutUp'
+                        }
+                    })
+                    navigate(redirectTo, { replace: true });
+                }
+
+                const userSaved = { name: loggedUser.displayName, email: loggedUser.email }
+
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(userSaved)
+                })
+                    .then(res => res.json())
+                    .then(() => {
+                    })
+            })
+    }
+
     return (
         <div className='flex items-center justify-between mx-10'>
             <h1 className="my-3 text-start">Or Login With</h1>
             <div className="flex justify-end">
                 <Link onClick={handleGoogleSignIn}><FaGoogle className="text-xl"></FaGoogle></Link>
+                <Link><button onClick={handleGithubSignIn}><FaGithub className="mx-2"></FaGithub></button></Link>
                 <Link><FaFacebook className="mx-5 text-xl"></FaFacebook></Link>
                 <Link><FaTwitter className='text-xl'></FaTwitter></Link>
-                <Link><FaGithub className='text-xl ml-5'></FaGithub></Link>
             </div>
         </div>
     );
